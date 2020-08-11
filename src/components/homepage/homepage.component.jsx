82a617bfb159153889a801d9bb4a724e234033e9
TheAstutian <speakingatoms@gmail.com>
2020-08-11 21:35:48 +0100

    updated homepage component

--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -14,6 +14,10 @@ class Homepage extends React.Component {
         return (
             <div>
                 <h1>Potterfans</h1>
+
+                Welcome to the PotterFans, an online resource and  spot for all things related to J.K. Rowling's Wizarding World. 
+                <span> Whether you're looking for info on Harry Potter characters, the book series or just wanna have fun, this is the place to be. </span>
+                
                 <span>This website is dedicated to harry potter fans</span>
             </div>
         )
