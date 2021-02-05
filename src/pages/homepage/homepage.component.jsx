e1e4bf378e0357bda9899225a22ed1284b00c858
TheAstutian <speakingatoms@gmail.com>
2021-02-05 17:31:10 +0100

    styled a tags

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import Banner from '../../components/Banner/banner.component';
-import {Button} from 'react-bootstrap';
+
 
 import './homepage.style.scss'; 
+import {Container} from 'react-bootstrap';
 
 
 const Homepage = () => (
-            <div>
+            <Container className='container'>
                <Banner />                
                 <span>This website is dedicated to harry potter fans- J. K. Rowling</span> 
-                <div className="section">
+                <div className="section lg-12 sm-1">
                     Visit Hogwarts
-                    <Button>Visit!</Button>
+                    <span>What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?</span>
                 </div>
                 <div className="section">
                     Books
@@ -26,7 +27,7 @@ const Homepage = () => (
                     
                 </div>
                 
-            </div>
+            </Container>
 )
 
 
