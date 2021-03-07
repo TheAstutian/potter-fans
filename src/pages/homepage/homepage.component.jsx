aecee1ac187e3795ca702c63854e783287d1cf32
TheAstutian <speakingatoms@gmail.com>
2021-03-07 16:06:20 +0100

    working on homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -11,13 +11,20 @@ const Homepage = () => (
                     
                         <Container className='container'>
                        <Banner />                
-                        <span>This website is dedicated to harry potter fans- J. K. Rowling</span> 
-                        <div className="section lg-12 sm-1">
-                            Visit Hogwarts
-                            <span>What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?</span>
+                        <span>Random popular harry potter quote</span> 
+                        <div className="pf-hogwarts section">
+                            <div className="banner-image" style ={{
+                                backgroundImage: `url(${'https://images.unsplash.com/photo-1569040029205-a03a8b455808?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1115&q=80'})`
+                                }}>
+                                    Oya naaa 
+                                    
+                                    <button>Go to Hogwarts</button>
+                            </div>
+                           
+                           
                         </div>
                         <div className="pf-books section">
-                            Books
+                            Books books books books 
                         </div>
                         <div className="pf-movies section">
                             Movies
