d8e90455421761aafa7c0670e046e6bb3bbc4e92
TheAstutian <speakingatoms@gmail.com>
2021-03-11 14:30:34 +0100

    added links to footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -9,11 +9,13 @@ const Footer = () => (
         <div className='links'>
                 <div>
                         <div className="link-entry">
-                            <span> Official sites</span>
+                            <span> Official HP Sites</span>
                             <ul>
-                                <li></li>
-                                <li></li>
-                                <li></li>
+                                <li><a href="https://www.jkrowling.com/"> J.K. Rowling </a></li>
+                                <li><a href="https://www.wizardingworld.com/"> Wizarding World </a></li>
+                                <li><a href="http://www.bloomsbury.com/harrypotter">Bloomsbury (books)</a></li>
+                                <li><a href="http://harrypotter.warnerbros.com/">Warner Bros (movies)</a></li>
+                                <li><a href='https://www.harrypotterfanzone.com/'>Fan Zone</a></li>
                             </ul>
 
                         </div>
@@ -21,9 +23,11 @@ const Footer = () => (
                         <div className="link-entry">
                             <span>Media</span>
                             <ul>
-                                <li></li>
-                                <li></li>
-                                <li></li>
+                                <li><a href="https://thedailyprophet.net/">The Daily Prophet</a></li>
+                                <li><a href="https://thequibs.com/">The Quibbler</a></li>
+                                <li><a href="https://www.pottertalk.net/">Potter Talk</a></li>
+                                <li><a href="https://www.mugglenet.com/">MuggleNet</a></li>
+                                <li><a href="http://www.potterdome.eu/">PotterDome</a></li>   
                             </ul>
 
                         </div>
@@ -31,18 +35,20 @@ const Footer = () => (
 
             <div>
                         <div className="link-entry">
-                            <span>Fan pages</span>
+                            <span>Forums & Communities</span>
                             <ul>
-                                <li></li>
-                                <li></li>
-                                <li></li>
+                                <li><a href="https://www.hexrpg.com/">Hogwarts Extreme</a></li>
+                                <li><a href=""></a></li>
+                                <li><a href="http://charmingrp.com/index.php">Charming</a></li>
+                                <li><a href="https://thebeginning11.proboards.com/">The Beginning of the end</a></li>
+                                <li><a href="https://elpida.jcink.net/">Elpida</a></li>
                             </ul>
 
                         </div>
                         <div className="link-entry">
                             <span>Fan art</span>
                             <ul>
-                                <li></li>
+                              <li><a href=""></a></li>
                                 <li></li>
                                 <li></li>
                             </ul>
@@ -54,18 +60,18 @@ const Footer = () => (
                         <div className="link-entry">
                             <span>Wiki</span>
                             <ul>
-                                <li></li>
-                                <li></li>
-                                <li></li>
+                                <li><a href='https://www.hp-lexicon.org/'> Harry Potter Lexicon</a></li>
+                                <li><a href='https://wikipotter-more.fandom.com/pt-br/wiki/Wiki_WikiPotter'>WikiPotter</a></li>    
+                                <li><a href="https://www.hogwartsbynight.com/">Hogwarts by Night</a></li>
+                                <li><a href="http://en.wikipedia.org/wiki/Portal:Harry_Potter">Wikipedia</a></li>
                             </ul>
 
                         </div>
                         <div className="link-entry">
                             <span>Charity</span>
                             <ul>
-                                <li></li>
-                                <li></li>
-                                <li></li>
+                                <li><a href="https://potterdome.eu/overons/potterdomeforprivacy/">Free Internet</a></li>
+                                
                             </ul>
 
                         </div>
