fe843c1e10a774438e1396e3b079eb030239867b
TheAstutian <speakingatoms@gmail.com>
2021-03-19 22:20:24 +0100

    header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React , {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {ReactComponent as CloseMenu} from "../../assets/x.svg";
 import {ReactComponent as MenuIcon} from "../../assets/menu.svg";
 
 import './header.style.scss';
 
-const Header = () =>(
-    <div className='header-container'>
-        <div className='header-font'>
-        <h1><Link to='/'>Potter World</Link></h1>
-        </div> 
+const Header = () => {
 
-        <div className='header-navs'>
-            <li><Link to='/movies'>MOVIES</Link></li>
-            <li><Link to='/books'>BOOKS</Link></li>
-            <li><Link to='hogwarts'> HOGWARTS</Link></li>
-            <li><Link to='spells'>SPELL BOOK</Link></li>
-            <li><Link to='sorting'>SORTING HAT</Link></li>
-        </div>
+            const [click, setClick] = useState(false);
+            const handleClick =() => setClick(!click);
+            const closeMobileMenu = () => setClick(false);
 
-        <CloseMenu />
-        <MenuIcon />
-
-    </div>
-)
+    return (
+                <div className='header-container'>
+                    <div className='header-font'>
+                        <h1><Link to='/'>Potter World</Link></h1>
+                    </div> 
+            
+                    <div className='header-navs'>
+                        <ul className={click? " nav-options active" : "nav-options"}>
+                            <li><Link to='/movies'>MOVIES</Link></li>
+                            <li><Link to='/books'>BOOKS</Link></li>
+                            <li><Link to='hogwarts'> HOGWARTS</Link></li>
+                            <li><Link to='spells'>SPELL BOOK</Link></li>
+                            <li><Link to='sorting'>SORTING HAT</Link></li>
+                        </ul>
+                    </div>
+            
+            
+                </div>
+        )
+}
     
 
 
