943c6ed54d9e909d6c26b9b76166beeae25ac7f4
TheAstutian <speakingatoms@gmail.com>
2021-04-02 22:03:21 +0100

    working on mobile menus

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -19,11 +19,11 @@ const Header = () => {
             
                     <div className='header-navs'>
                         <ul className={click? " nav-options active" : "nav-options"}>
-                            <li className="option" onClick={closeMobileMenu}><Link to='/movies'>MOVIES</Link></li>
-                            <li className="option" onClick={closeMobileMenu}><Link to='/books'>BOOKS</Link></li>
-                            <li className="option" onClick={closeMobileMenu}><Link to='hogwarts'> HOGWARTS</Link></li>
-                            <li className="option" onClick={closeMobileMenu}><Link to='spells'>SPELL BOOK</Link></li>
-                            <li className="option" onClick={closeMobileMenu}><Link to='sorting'>SORTING HAT</Link></li>
+                            <li className="option"> <Link to='sorting'>SORTING HAT</Link></li>
+                            <li className="option" > <Link to='/movies'>MOVIES</Link></li>
+                            <li className="option" > <Link to='/books'>BOOKS</Link></li>
+                            <li className="option" > <Link to='hogwarts'> HOGWARTS</Link></li>
+                            <li className="option" > <Link to='spells'>SPELL BOOK</Link></li>
                         </ul>
                     </div>
 
