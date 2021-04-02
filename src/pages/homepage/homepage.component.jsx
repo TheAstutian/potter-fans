6dbf209c39bafd2bf4d680594e0fe36324ee44e6
TheAstutian <speakingatoms@gmail.com>
2021-04-02 21:47:35 +0100

    hp changes

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -14,7 +14,9 @@ const Homepage = () => (
                        <div className='experiment'>
 
                             <div className='banner-text'>
-                                <span>We’ve all got both light and dark inside us.</span><span> What matters is the part we choose to act on. </span><span>That’s who we really are.</span>                                     
+                                <span>We’ve all got both light and dark inside us.</span>
+                                <span> What matters is the part we choose to act on. </span>
+                                <span>That’s who we really are.</span>                                     
                                 </div>
                         
                         </div> 
@@ -22,13 +24,16 @@ const Homepage = () => (
                         <div className=" section pf-hogwarts" style ={{
                             backgroundImage: `url(${'/banner_img.jpg'})`
                             }}>
-                                <span>We’ve all got both light and dark inside us.</span><span> What matters is the part we choose to act on. </span><span>That’s who we really are.</span>                                     
+                                
                         </div>
                         
                            
                         <span>Explore the world of Wizardry!</span> 
                         <div className="pf-books section">
-                            Books books books books 
+                            <div>
+                                Image 
+                            </div>
+                            <div> text</div>
                         </div>
 
                         <div className="pf-movies section">
