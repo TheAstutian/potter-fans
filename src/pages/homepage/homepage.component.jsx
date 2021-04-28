9f657e5cfed1174aff95ed9cee1f3fba71f56772
TheAstutian <speakingatoms@gmail.com>
2021-04-28 22:57:34 +0100

    styling homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -14,8 +14,8 @@ const Homepage = () => (
                        <Banner />                
                        <div className='experiment'>
 
-                            <div className='banner-2'>
-                                <span>We’ve all got both light and dark inside us.</span>
+                            <div className='banner-1'>
+                                <span>We’ve all got both light and darkness inside us.</span>
                                 <span> What matters is the part we choose to act on. </span>
                                 <span>That’s who we really are.</span>  
                                 <button><Link to='/sorting'>Sorting Hat</Link></button>                                   
@@ -28,11 +28,11 @@ const Homepage = () => (
                                                 
  
                         <div className='movie-book'>
-                            <div className="pf-books">
-                                <div>
-                                    <img src='https://images.pexels.com/photos/1005012/pexels-photo-1005012.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260' />
-                                </div>
-                                <div> <button><Link to='/books'>Harry Potter Books</Link></button></div>
+                            <div className="pf-books"  style ={{
+                            backgroundImage: `url(${'https://images.pexels.com/photos/1005012/pexels-photo-1005012.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'})`
+                            }}>
+                               
+                                <div className='le-button'> <button><Link to='/books'>Harry Potter Books</Link></button></div>
                             </div>
 
                             <div className="pf-movies ">
@@ -51,9 +51,11 @@ const Homepage = () => (
                         <div className="jk-rowling ">
                             <h1>Who is <a href='https://www.jkrowling.com/'> J. K. Rowlings? </a></h1>
                             <img src='https://media3.s-nbcnews.com/i/newscms/2020_24/3386271/200610-jk-rowling-al-1348_c9a1eed578999b876d0ef072061a87c3.jpg' />
-                            <p>Joanne Rowling was born on 31st July 1965 at Yate General Hospital near Bristol, and grew up in Gloucestershire in England and in Chepstow, Gwent, in south-east Wales.</p>
-                            <p>The “K” stands for Kathleen, her paternal grandmother’s name. It was added at her publisher’s request, who thought a book by an obviously female author might not appeal to the target audience of young boys.</p>
-                            <p>She is the author of the much-loved series of seven Harry Potter novels, originally published between 1997 and 2007. Along with the three companion... <a href='https://www.jkrowling.com/about/'>more</a></p> 
+                            <div className='jk-text'>
+                                <p>Joanne Rowling was born on 31st July 1965 at Yate General Hospital near Bristol, and grew up in Gloucestershire in England and in Chepstow, Gwent, in south-east Wales.</p>
+                                <p>The “K” stands for Kathleen, her paternal grandmother’s name. It was added at her publisher’s request, who thought a book by an obviously female author might not appeal to the target audience of young boys.</p>
+                                <p>She is the author of the much-loved series of seven Harry Potter novels, originally published between 1997 and 2007. Along with the three companion... <a href='https://www.jkrowling.com/about/'> read more</a></p> 
+                            </div>
                             
                         </div>
                         
