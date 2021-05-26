a7217a5178b82ed8b6060e02179ef3d950600b5a
TheAstutian <speakingatoms@gmail.com>
2021-05-26 11:52:27 +0100

    prepping bookpage

--- src/pages/books/bookpage.component.jsx
+++ src/pages/books/bookpage.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react'; 
 
-const BookPage = () => (
-         <div>
-                 <h2>Harry Potter Books</h2>
-        </div>  
+class BookPage extends Component {
+        state={
+
+        }
+
+        render(){
+        return(
+                <div>
+                        <h2>Harry Potter Books</h2>
+                </div>  
         )
+        }
+}
    
 
 export default BookPage; 
