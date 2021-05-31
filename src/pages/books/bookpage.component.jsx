f29a986d505ce3d246f749902f56117b9f40d330
TheAstutian <speakingatoms@gmail.com>
2021-05-31 19:58:54 +0100

    gotten hang for books

--- src/pages/books/bookpage.component.jsx
+++ src/pages/books/bookpage.component.jsx
@@ -2,13 +2,41 @@ import React from 'react';
 
 class BookPage extends Component {
         state={
-
+                loading: false,
+                books: []
         }
+
+
+
+        componentDidMount(){
+		this.setState({loading:true});
+		const endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${"Harry potter"}`;
+		const endpointbk2= 'https://www.googleapis.com/books/v1/volumes/Wlp0nAEACAAJ';
+		this.fetchItems(endpoint);
+	}
+
+        fetchItems =(endpoint) =>{
+                fetch(endpoint)
+                .then(result=>result.json())
+                .then(result=>{
+                        console.log(result);
+                        this.setState({
+                                movies: [...this.state.movies, ...result.results],
+                                heroImage: this.state.heroImage || result.results[0],
+                                loading: false,
+                                currentPage: result.page,
+                                totalPages: result.total_pages
+                        })
+                })
+        }
+        
 
         render(){
         return(
                 <div>
                         <h2>Harry Potter Books</h2>
+
+
                 </div>  
         )
         }
