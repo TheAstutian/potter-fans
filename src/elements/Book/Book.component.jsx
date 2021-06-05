80a1ffd90f1922c482bf3855d6f880ba05e48ca3
TheAstutian <speakingatoms@gmail.com>
2021-06-05 12:50:36 +0100

    book page working on

---
+++ src/elements/Book/Book.component.jsx
@@ -0,0 +1,33 @@
+import react from 'react';
+
+import '../Book.style.scss'
+
+const Book = ()=>{
+    return(
+        <div>
+            <div>
+            <p>Title</p>
+            <p>Authors:</p>
+            </div>
+            <div>
+                <div>
+                <p>Thumb image</p>
+                </div>
+                <div>
+                    Description
+
+                    Add link for preview
+                </div>
+            </div>
+            <div>
+               Published by: Publisher
+               Released: Published date
+               Rating:
+                
+
+            </div>
+        </div>
+    )
+}
+
+export default Book;
