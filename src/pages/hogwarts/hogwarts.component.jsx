22e7cfe0850851faa7560fde568a252582f879fd
TheAstutian <speakingatoms@gmail.com>
2021-07-11 20:42:07 +0100

    resuming project

--- src/pages/hogwarts/hogwarts.component.jsx
+++ src/pages/hogwarts/hogwarts.component.jsx
@@ -6,22 +6,20 @@ import './hogwarts.style.scss'
 
 const Hogwarts = () => (
          <div className='hogwarts-container'>
-             <Banner imagelink='https://cdn.pixabay.com/photo/2015/09/20/22/38/library-948965_1280.jpg' content='Welcome to Hogwarts!'/>                
+             <Banner imagelink='https://cdn.pixabay.com/photo/2015/09/20/22/38/library-948965_1280.jpg' content='School of Wizardry'/>                
+             
              <div>
-             <span>The School of Wizardry!</span>
-             <p>Welcome to Hogwarts, School of Wizardry</p>
-             </div>
+                Houses. Square with color of house as background.
+            </div>
             
             <div>
-             <h2>These are the staff of Hogwarts</h2>
+
+             <h2>Hogwart Staff</h2>
                 <FaceCollection />
             </div>
             <div>
                 Sorting hat. Have you been sorted?
                 Or Just import sorting hat Component
-            </div>
-            <div>
-                Houses. Square with color of house as background.
             </div>
 
          </div>
