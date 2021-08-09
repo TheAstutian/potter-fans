953e6dab80bb6441a2e7a136f72935926e03d889
TheAstutian <speakingatoms@gmail.com>
2021-08-09 13:31:57 +0100

    sorting hat functionality done

--- src/pages/sortinghat/sortinghat.component.jsx
+++ src/pages/sortinghat/sortinghat.component.jsx
@@ -5,21 +5,66 @@ const houses = [
     "Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"
 ]
 
+const greetings = [
+    "Hey", "Hello", "Hmmmm......", "Hi"
+]
+
+const addendum = [
+    "Congratulations",
+    "Yee-haw",
+    "Well done",
+    "Congrats"
+]
+
+const messages = [
+    "I see you doing wonders at",
+    "your destiny lies at",
+    "you are best suited for",
+    "I am certain you belong to",
+    "your path lies at",
+    "your personality will fit in at",
+    "people like you belong to"
+]
+
 class SortingHat extends React.Component{
     state = {
         active:false,
         sorted_house:"",
         name:"",
+        greeting:"",
+        message:"",
+        addendum:"",
     }
 
     componentDidMount(){
         this.setState({active:false})
     }
+
+    random=()=>{
+        const randomNum = Math.floor((Math.random() * 100) + 1) // Generates number between 1 and 100
+        return randomNum;
+    }
+
+    pick_greeting=()=>{
+        const randomNum = this.random()
+        const greeting = greetings[randomNum % greetings.length]
+        return greeting
+    }
+
+    pick_message=()=>{
+        const message = messages[this.random() % messages.length]
+        return message
+    }
+
+    pick_addendum=()=>{
+        return addendum[this.random() % addendum.length]
+    }
+
     pick_house = ()=>{
         // check if name is valid first
         
-        const randomNum = Math.floor((Math.random() * 100) + 1) // Generates number between 1 and 100
-        const houseName = houses[randomNum % 4]
+        const randomNum = this.random()
+        const houseName = houses[randomNum % houses.length]
         
         return houseName
     }
@@ -28,7 +73,22 @@ class SortingHat extends React.Component{
 
         const house = this.pick_house()
         this.setState({sorted_house:house})
-        this.setState({active:true})
+        const greeting = this.pick_greeting()
+        const message = this.pick_message()
+        this.setState({active:true, greeting:greeting, message:this.pick_message(),addendum:this.pick_addendum()})
+    }
+
+    handleChange= (e)=>{
+        const user_name = e.target.value;
+        this.setState({name:user_name})
+        this.setState({active:false})
+    }
+    reset = ()=>{
+        this.setState({active:false, name:"",active:false,
+        sorted_house:"",
+        greeting:"",
+        message:"",
+        addendum:"",})
     }
 
     render(){
@@ -41,14 +101,18 @@ class SortingHat extends React.Component{
                 </div>
                 <div>
                   <input 
+                        type="text"
                         placeholder="What is your name, Muggle?"
-					    value={this.state.name}
+                        onChange = {this.handleChange}
+					    value = {this.state.name}
                     />
                     <button onClick={()=>this.onSubmit()}>Get sorted</button>
                 </div>
 
-                <div style={{"color":"white"}}>
-                   { this.state.active===true ? (<p> {this.state.name}, You are in {this.state.sorted_house}</p>): null }
+                <div style={{color:"white"}}>
+                   { this.state.active===true ? (<div>
+                    <p> {this.state.greeting} {this.state.name}, {this.state.message} {this.state.sorted_house}. {this.state.addendum}!</p> <button onClick={()=>this.reset()}>Reset</button>
+                   </div>): null }
                 </div>
             </div>
         )
