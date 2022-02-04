c0b7885332897144ae52c9feebabc35876e79b1b
TheAstutian <speakingatoms@gmail.com>
2022-02-04 23:55:22 +0100

    ..

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -63,11 +63,11 @@ const Homepage = () => (
                         </div>
 
                         <div className="jk-rowling ">
-                            <h1>Who is <a href='https://www.jkrowling.com/'> J. K. Rowling? </a></h1>
+                            <h1>About <a href='https://www.jkrowling.com/'> J. K. Rowling </a></h1>
                             <img src='https://media3.s-nbcnews.com/i/newscms/2020_24/3386271/200610-jk-rowling-al-1348_c9a1eed578999b876d0ef072061a87c3.jpg' />
                             <div className='jk-text'>
-                                <p>Joanne Rowling was born on 31st July 1965 at Yate General Hospital near Bristol, and grew up in Gloucestershire in England and in Chepstow, Gwent, in south-east Wales.</p>
-                                <p>The “K” stands for Kathleen, her paternal grandmother’s name. It was added at her publisher’s request, who thought a book by an obviously female author might not appeal to the target audience of young boys.</p>
+                                <p>Joanne Rowling was born on 31st July 1965 at Yate General Hospital near Bristol, and grew up in Gloucestershire in England and in Chepstow, Gwent, in south-east Wales.
+                                The “K” stands for Kathleen, her paternal grandmother’s name. It was added at her publisher’s request, who thought a book by an obviously female author might not appeal to the target audience of young boys.</p>
                                 <p>She is the author of the much-loved series of seven Harry Potter novels, originally published between 1997 and 2007. Along with the three companion... <a href='https://www.jkrowling.com/about/'> read more</a></p> 
                             </div>
                             
