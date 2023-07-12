a5bc759c24ab0328d5b79e0a884e93928361829f
TheAstutian <speakingatoms@gmail.com>
2023-07-12 22:16:15 +0100

    worked on JK'ss about section on Homepge.js

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-import React , {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom'
 
 import Banner from '../../components/Banner/banner.component';
@@ -12,17 +12,17 @@ const Homepage = () => (
                     
             <div className='container'>
                        <Banner 
-                       main_content="WELCOME TO THE WORLD OF MAGIC!"
+                       main_content="WelcOmE  To The WoRlD Of MAGIC!"
                        subcontent='"It is our choices that show what we truly are, far more than our abilities" '
                        subcontent2="- Albus Dumbledore"
                         />                
                        <div className='experiment'>
 
                             <div className='banner-1'>
-                                <h2>CHOOSE YOUR DESTINY !</h2>
+                                <h2>CHOOSE YOuR DESTiNY !</h2>
                                     <div className='content'>
                                         <div className='content-image'>
-                                        <img className= 'img' src='https://media.tenor.com/4vuIoALDXNwAAAAC/sorting-hat-harry-potter.gif' />  
+                                        <img className= 'img' src='https://media.tenor.com/4vuIoALDXNwAAAAC/sorting-hat-harry-potter.gif' alt='2'/>  
                                         </div>
                                         <div className='content-body'>
                                        
@@ -86,12 +86,12 @@ const Homepage = () => (
 
 
                         <div className="jk-rowling ">
-                            <h1>About <a href='https://www.jkrowling.com/'> J. K. Rowling </a></h1>
-                            <img src='https://media3.s-nbcnews.com/i/newscms/2020_24/3386271/200610-jk-rowling-al-1348_c9a1eed578999b876d0ef072061a87c3.jpg' />
+                            <h1>About <a href='https://www.jkrowling.com/about'> J. K. Rowling </a></h1>
+                            <img src='https://media3.s-nbcnews.com/i/newscms/2020_24/3386271/200610-jk-rowling-al-1348_c9a1eed578999b876d0ef072061a87c3.jpg' alt='5'/>
                             <div className='jk-text'>
-                                <p>Joanne Rowling was born on 31st July 1965 at Yate General Hospital near Bristol, and grew up in Gloucestershire in England and in Chepstow, Gwent, in south-east Wales.
-                                The “K” stands for Kathleen, her paternal grandmother’s name. It was added at her publisher’s request, who thought a book by an obviously female author might not appeal to the target audience of young boys.</p>
-                                <p>She is the author of the much-loved series of seven Harry Potter novels, originally published between 1997 and 2007. Along with the three companion... <a href='https://www.jkrowling.com/about/'> read more</a></p> 
+                                <span>Joanne Rowling was born on 31st July 1965 at Yate General Hospital near Bristol, and grew up in Gloucestershire in England and in Chepstow, Gwent, in south-east Wales.</span>
+                                <span>The “K” stands for Kathleen, her paternal grandmother’s name. It was added at her publisher’s request, who thought a book by an obviously female author might not appeal to the target audience of young boys.</span>
+                                <span>She is the author of the much-loved series of seven Harry Potter novels, originally published between 1997 and 2007. Along with the three companion... <a href='https://www.jkrowling.com/about/'> read more</a></span> 
                             </div>
                             
                         </div>
