38427b1c0eba3030ff377b8baedc353364bd9a93
TheAstutian <speakingatoms@gmail.com>
2023-08-06 22:27:22 +0100

    potioninfo.css

--- src/components/PotionInfo/PotionInfo.jsx
+++ src/components/PotionInfo/PotionInfo.jsx
@@ -17,22 +17,22 @@ const PotionInfo=({show, data, handleClose})=>{
               <div className='top'>
                     <h3>{name}</h3>
                     <img src={image? `${image}` : no_image} />
+                    <p>{effect}</p>
               </div>
-             
-               <p>{effect}</p>
                <div className='bottom'>
                 <h3>More Info</h3>
-                <div className='ppt'>  <span>Effect:</span><span className="ans"> {effect? effect: "N/A"}</span></div>
-                <div className='ppt'>  <span>Side Effects: </span><span className="ans"> {side_effects? side_effects: "N/A"}</span></div>
-                <div className='ppt'> <span>Characteristics: </span><span className="ans"> {characteristics? characteristics: "N/A" }</span></div>
-                <div className='ppt'></div><div className='ppt'> <span>Ingredients:</span><span className="ans">  {ingredients? ingredients: "N/A"}</span></div>
-                <div className='ppt'>  <span>Difficulty: </span><span className="ans"> {difficulty? difficulty: "N/A"}</span></div>
-                <div className='ppt'>  <span>Inventors:</span><span className="ans">  {inventors? inventors: "N/A" }</span></div>
-                <div className='ppt'> <span>Manufacturers: </span><span className="ans"> {manufacturers? manufacturers: "N/A" }</span></div>
-                <div className='ppt'> <span>Time Used: </span><span className="ans"> {time? time: "N/A" }</span></div>
+                <div className='ppt'>  <span className="que">Side Effects: </span><span className="ans"> {side_effects? side_effects: "N/A"}</span></div>
+                <div className='ppt'> <span className="que">Characteristics: </span><span className="ans"> {characteristics? characteristics: "N/A" }</span></div>
+                <div className='ppt'> <span className="que">Ingredients:</span><span className="ans">  {ingredients? ingredients: "N/A"}</span></div>
+                <div className='ppt'>  <span className="que">Difficulty: </span><span className="ans"> {difficulty? difficulty: "N/A"}</span></div>
+                <div className='ppt'>  <span className="que">Inventors:</span><span className="ans">  {inventors? inventors: "N/A" }</span></div>
+                <div className='ppt'> <span className="que">Manufacturers: </span><span className="ans"> {manufacturers? manufacturers: "N/A" }</span></div>
+                <div className='ppt'> <span className="que">Time Used: </span><span className="ans"> {time? time: "N/A" }</span></div>
                 
                </div>
-                 <button ><a href={wiki} target='blank'>Wikipage</a></button>
+                <div className='wiki'>
+                <button className="wiki-btn"><a href={wiki} target='blank'>Wikipage</a></button>
+                </div>
             </div>
            
         </div>
